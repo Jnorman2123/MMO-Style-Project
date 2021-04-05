bcc979a084c4f7eba060e00c837299d3c1babd7b
Jonathan Norman <Jnorman2123@gmail.com>
2021-04-05 15:19:50 -0400

    Added a rigidbody and movement script to the player
    Added a rigidbody to the player, created a movement script and added to the player

---
+++ MMO-Style-Project/Assets/Scripts/Movement.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    // Declare variables movementSpeed and turnSpeed
+    public float movementSpeed;
+    public float turnSpeed;
+    // Declare variables for player input
+    private float verticalInput;
+    private float horinzontalInput;
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Set the movement speed based on the game object
+        if (gameObject.CompareTag("Player"))
+        {
+            movementSpeed = 10.0f;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    // Create function to move the player based on the key pressed
+    private void Move(float movementSpeed)
+    {
+        // Move the player based on the key pressed
+        if (Input.GetKeyDown(KeyCode.W) & gameObject.CompareTag("Player"))
+        {
+            gameObject.transform.forward
+        }
+    }
+}
