9993dc060b0e0c710c88fcb5834ed7a63ef57bd1
Jonathan Norman <Jnorman2123@gmail.com>
2021-04-05 22:20:32 -0400

    Player can move by simply holding both mouse buttons
    When the player holds both right and left mouse buttons the player will automatically move forward and then can strafe by using horizontal axis buttons.

--- MMO-Style-Project/Assets/Scripts/Movement.cs
+++ MMO-Style-Project/Assets/Scripts/Movement.cs
@@ -5,12 +5,13 @@ using UnityEngine;
 public class Movement : MonoBehaviour
 {
     // Declare and set variables movementSpeed and turnSpeed and turnPlayer
-    public float movementSpeed = 10.0f;
-    public float turnSpeed = 20.0f;
+    private float movementSpeed = 10.0f;
+    private float turnSpeed;
     private float turnPlayer = 0.0f;
     // Declare variables for player input
     private float verticalInput;
     private float horizontalInput;
+    private float horizontalMouse;
     // Declare variable for rigidbody
     private Rigidbody objectRB;
     // Start is called before the first frame update
@@ -32,6 +33,8 @@ public class Movement : MonoBehaviour
         // Set inputs to vertical and horizontal input
         verticalInput = Input.GetAxisRaw("Vertical");
         horizontalInput = Input.GetAxisRaw("Horizontal");
+        // Set mouse axis input
+        horizontalMouse = Input.GetAxisRaw("Mouse X");
         // Set movement based on object
         if (gameObject.CompareTag("Player"))
         {
@@ -39,13 +42,35 @@ public class Movement : MonoBehaviour
             Vector3 horizontalMovement = new Vector3(horizontalInput, 0.0f, 0.0f);
             // Move character based on input
             transform.Translate(verticalMovement * Time.deltaTime * movementSpeed);
-            // Check if the left mouse button is pushed down to determine type of movement with horizontal input
-            if (Input.GetMouseButton(1))
+            // Check which mouse button is pushed down to determine type of movement with horizontal input
+            if (Input.GetMouseButton(0) & Input.GetMouseButton(1))
             {
+                // Set foward movement of player
+                Vector3 moveForward = new Vector3(0.0f, 0.0f, 1);
+                // Set turnSpeed
+                turnSpeed = 300.0f;
+                // Move player forward
+                transform.Translate(moveForward * Time.deltaTime * movementSpeed);
+                // Turn player based on horizontal mouse input
+                turnPlayer += horizontalMouse * turnSpeed * Time.deltaTime;
+                transform.eulerAngles = new Vector3(0.0f, turnPlayer, 0.0f);
                 // Strafe charace side to side based on input
                 transform.Translate(horizontalMovement * Time.deltaTime * movementSpeed);
-            } else
+            }
+            if (Input.GetMouseButton(1))
             {
+                // Set turnSpeed
+                turnSpeed = 300.0f;
+                // Turn player based on horizontal mouse input
+                turnPlayer += horizontalMouse * turnSpeed * Time.deltaTime;
+                transform.eulerAngles = new Vector3(0.0f, turnPlayer, 0.0f);
+                // Strafe charace side to side based on input
+                transform.Translate(horizontalMovement * Time.deltaTime * movementSpeed);
+            } 
+            if (!Input.GetMouseButton(1)) 
+            {
+                // Set turnSpeed
+                turnSpeed = 100.0f;
                 // Turn charatcter based on input
                 turnPlayer += horizontalInput * turnSpeed * Time.deltaTime;
                 transform.eulerAngles = new Vector3(0.0f, turnPlayer, 0.0f);
