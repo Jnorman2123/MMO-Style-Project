64c7423273df345e1048a68a78ab2c9d2afd3fdf
Jonathan Norman <Jnorman2123@gmail.com>
2021-04-06 16:01:15 -0400

    Added method to determine when the camera is in first person view
    Added method that checks to see if the camera is in the first or third person

--- MMO-Style-Project/Assets/Scripts/CameraController.cs
+++ MMO-Style-Project/Assets/Scripts/CameraController.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
+    // Declare a variable to determine if in first or third person
+    public bool firstPerson;
     // Declare variable for the first person camera game object
     public Transform firstPersonCamera;
     // Declare variable for mouse scroll wheel input
@@ -17,7 +19,6 @@ public class CameraController : MonoBehaviour
     {
         cameraOffset = new Vector3(0, 0, 0);
     }
-
     // Update is called once per frame
     void Update()
     {
@@ -28,24 +29,47 @@ public class CameraController : MonoBehaviour
     {
         // Call ZoomCamera method
         ZoomCamera();
-        // Set the position of the camera to an offset of the player position
-        transform.position = firstPersonCamera.position + cameraOffset;
-        // Set the rotation of the camera to equal the rotation of the player
-        transform.rotation = firstPersonCamera.rotation;
+        // Call the FollowPlayer method
+        FollowPlayer();
+        // Call the IsFirstPerson method
+        IsFirstPerson();
     }
     // Method to zoom the camera in and out with the mouse wheel
     private void ZoomCamera()
     {
         // Set the cameraMoveSpeed
-        //cameraMoveSpeed = 10.0f;
+        cameraMoveSpeed = 10.0f;
         // When scrolling the mouse wheel zoom the camera from first person to third person
         mouseScrollWheel = Input.GetAxis("Mouse ScrollWheel");
         // Set camera offset move
         Vector3 offsetMove = new Vector3(0, 0, mouseScrollWheel);
-        Debug.Log(mouseScrollWheel);
         // Make camera look the firstPersonCamera object
         transform.LookAt(firstPersonCamera);
         // Move the camera in and out with the mouse wheel
         cameraOffset += offsetMove;
+    }
+    // Method to turn the camera
+    private void TurnCamera()
+    {
+
+    }
+    // Method to follow the player with camera
+    private void FollowPlayer()
+    {
+        // Set the position of the camera to an offset of the player position
+        transform.position = firstPersonCamera.position + cameraOffset;
+        // Set the rotation of the camera to equal the rotation of the player
+        transform.rotation = firstPersonCamera.rotation;
+    }
+    // Method to change firstPerson to true or false based on camera position
+    private void IsFirstPerson()
+    {
+        if (cameraOffset == new Vector3(0,0,0))
+        {
+            firstPerson = true;
+        } else
+        {
+            firstPerson = false;
+        }
     }
 }
