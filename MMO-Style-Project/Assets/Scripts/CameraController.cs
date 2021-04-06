e67cac01b8366c6c163f9102febc33e0f498e285
Jonathan Norman <Jnorman2123@gmail.com>
2021-04-06 14:36:09 -0400

    Added ability to zoom the camera in and out with the mouse wheel
    By scrolling the mouse wheel in and out the player to move the camera backward and forward.

--- MMO-Style-Project/Assets/Scripts/CameraController.cs
+++ MMO-Style-Project/Assets/Scripts/CameraController.cs
@@ -5,12 +5,17 @@ using UnityEngine;
 public class CameraController : MonoBehaviour
 {
     // Declare variable for the first person camera game object
-    private GameObject firstPersonCamera;
+    public Transform firstPersonCamera;
+    // Declare variable for mouse scroll wheel input
+    private float mouseScrollWheel;
+    // Declare variable for camera move speed
+    private float cameraMoveSpeed;
+    // Declare variable for camera offset
+    private Vector3 cameraOffset;
     // Start is called before the first frame update
     void Start()
     {
-        // Set the player gameobject
-        firstPersonCamera = GameObject.FindGameObjectWithTag("FPC");
+        cameraOffset = new Vector3(0, 0, 0);
     }
 
     // Update is called once per frame
@@ -21,9 +26,26 @@ public class CameraController : MonoBehaviour
     // Use a late update
     private void LateUpdate()
     {
+        // Call ZoomCamera method
+        ZoomCamera();
         // Set the position of the camera to an offset of the player position
-        gameObject.transform.position = firstPersonCamera.transform.position;
+        transform.position = firstPersonCamera.position + cameraOffset;
         // Set the rotation of the camera to equal the rotation of the player
-        gameObject.transform.rotation = firstPersonCamera.transform.rotation;
+        transform.rotation = firstPersonCamera.rotation;
+    }
+    // Method to zoom the camera in and out with the mouse wheel
+    private void ZoomCamera()
+    {
+        // Set the cameraMoveSpeed
+        //cameraMoveSpeed = 10.0f;
+        // When scrolling the mouse wheel zoom the camera from first person to third person
+        mouseScrollWheel = Input.GetAxis("Mouse ScrollWheel");
+        // Set camera offset move
+        Vector3 offsetMove = new Vector3(0, 0, mouseScrollWheel);
+        Debug.Log(mouseScrollWheel);
+        // Make camera look the firstPersonCamera object
+        transform.LookAt(firstPersonCamera);
+        // Move the camera in and out with the mouse wheel
+        cameraOffset += offsetMove;
     }
 }
