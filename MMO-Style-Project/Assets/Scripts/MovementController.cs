2370028f7c31fadbdf3dfab49f25c4c62949dc04
Jonathan Norman <Jnorman2123@gmail.com>
2021-04-08 21:29:36 -0400

    Player can now strafe with the horizontal input while holding the right mouse button
    The player can strafe side to side with the horizontal input while holding the right mouse button

--- MMO-Style-Project/Assets/Scripts/MovementController.cs
+++ MMO-Style-Project/Assets/Scripts/MovementController.cs
@@ -4,14 +4,17 @@ using UnityEngine;
 
 public class MovementController : MonoBehaviour
 {
-    // Declare variables for move and turn input
+    // Declare variables for move input, turn input, and mouse x input
     private float moveInput;
     private float turnInput;
+    private float mouseXInput;
     // Declare variables for move speed and turn speed
-    public float moveSpeed;
-    public float turnSpeed;
-    // Declare variable for turn direction
+    private float moveSpeed = 15f;
+    private float turnSpeed;
+    // Declare variables for turn direction, move direction, and strafe direction
     private float turnDirection;
+    private Vector3 moveDirection;
+    private Vector3 strafeDirection;
     // Start is called before the first frame update
     void Start()
     {
@@ -31,19 +34,49 @@ public class MovementController : MonoBehaviour
     {
         // Set the moveInput variable to the vertical axis input
         moveInput = Input.GetAxisRaw("Vertical");
-        // Set the direction to move based on the vertical input
-        Vector3 moveDirection = new Vector3(0.0f, 0.0f, moveInput);
-        // Translate the player based on move direction and move speed
-        transform.Translate(moveDirection * moveSpeed * Time.deltaTime);
+        // Set the turnInput variable to the horizontal axis input
+        turnInput = Input.GetAxisRaw("Horizontal");
+        // Set the moveDirection based on the moveInput
+        moveDirection = new Vector3(0.0f, 0.0f, moveInput);
+        // Set the strafeDirection based on the turnInput
+        strafeDirection = new Vector3(turnInput, 0.0f, 0.0f);
+        // Check if the right mouse button is clicked and make character strafe based on turnInput 
+        if (Input.GetMouseButton(1))
+        {
+            // Translate the player side to side and forward back based on moveDirection and strafeDirection
+            transform.Translate(strafeDirection * moveSpeed * Time.deltaTime);
+            transform.Translate(moveDirection * moveSpeed * Time.deltaTime);
+        } else
+        {
+            // Translate the player based on moveDirection
+            transform.Translate(moveDirection * moveSpeed * Time.deltaTime);
+        }
+        
     }
     // Method to turn the character based on horizontal input
     private void Turn()
     {
         // Set the turnInput variable to the horizontal axis input
         turnInput = Input.GetAxisRaw("Horizontal");
-        // Set the turnDirection based on the turnInput and turn speed
-        turnDirection += turnInput * turnSpeed * Time.deltaTime;
+        // Set the mouseXInput variable to the mouse X axis
+        mouseXInput = Input.GetAxis("Mouse X");
+        // Check if the right mouse button is being held and set the turnDirection and turnSpeed accordingly
+        if (Input.GetMouseButton(1))
+        {
+            // Set the turnSpeed
+            turnSpeed = 150f;
+            // Set the turnDirection based on the mouseXInput
+            turnDirection += mouseXInput * turnSpeed * Time.deltaTime;            
+        } else
+        {
+            // Set the turnSpeed
+            turnSpeed = 75f;
+            // Set the turnDirection based on the turnInput and turn speed
+            turnDirection += turnInput * turnSpeed * Time.deltaTime;
+        }
         // Turn the player based on turnDirection
-        transform.eulerAngles = new Vector3(0.0f, turnDirection, 0.0f);   
+        transform.eulerAngles = new Vector3(0.0f, turnDirection, 0.0f);
     }
+    // Method to make the character jump when space bar is pressed
+    
 }
