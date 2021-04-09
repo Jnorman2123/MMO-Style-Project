21c73c2d5ea0c49c92a938be53fe4f0c0e9528ac
Jonathan Norman <Jnorman2123@gmail.com>
2021-04-08 20:42:54 -0400

    Player can now move and turn with the vertical and horizontal inputs
    The player can move and turn with the vertical and horizontal inputs again

--- MMO-Style-Project/Assets/Scripts/MovementController.cs
+++ MMO-Style-Project/Assets/Scripts/MovementController.cs
@@ -10,6 +10,8 @@ public class MovementController : MonoBehaviour
     // Declare variables for move speed and turn speed
     public float moveSpeed;
     public float turnSpeed;
+    // Declare variable for turn direction
+    private float turnDirection;
     // Start is called before the first frame update
     void Start()
     {
@@ -21,6 +23,8 @@ public class MovementController : MonoBehaviour
     {
         // Call the Move method
         Move();
+        // Call the turn method
+        Turn();
     }
     // Method to move the character based on vertical input
     private void Move()
@@ -31,5 +35,15 @@ public class MovementController : MonoBehaviour
         Vector3 moveDirection = new Vector3(0.0f, 0.0f, moveInput);
         // Translate the player based on move direction and move speed
         transform.Translate(moveDirection * moveSpeed * Time.deltaTime);
+    }
+    // Method to turn the character based on horizontal input
+    private void Turn()
+    {
+        // Set the turnInput variable to the horizontal axis input
+        turnInput = Input.GetAxisRaw("Horizontal");
+        // Set the turnDirection based on the turnInput and turn speed
+        turnDirection += turnInput * turnSpeed * Time.deltaTime;
+        // Turn the player based on turnDirection
+        transform.eulerAngles = new Vector3(0.0f, turnDirection, 0.0f);   
     }
 }
