d3d37de3c2b91c6f40a9085ac8c4db9d18aaba0d
Jonathan Norman <Jnorman2123@gmail.com>
2021-04-12 21:10:52 -0400

    The transition from first person to third person and back can now be triggered just by zooming in or out with the mouse wheel
    The transition from first person to third person and back can now be triggered just by zooming in or out with the mouse wheel

--- MMO-Style-Project/Assets/Scripts/CameraController.cs
+++ MMO-Style-Project/Assets/Scripts/CameraController.cs
@@ -8,13 +8,46 @@ public class CameraController : MonoBehaviour
     // Declare variables for the first and third person cameras
     public CinemachineVirtualCamera firstPersonCamera;
     public CinemachineFreeLook thirdPersonCamera;
+    // Declare variable for the thirdPersonCamera orbits
+    private CinemachineFreeLook.Orbit[] tpcOrbits;
+    // Set the range and base value of the minimum and maximum orbit scale
+    [Range(0.01f, 1f)]
+    public float minScale = 0.5f;
+    [Range(1F, 5f)]
+    public float maxScale = 1;
+    // Set the axis state and how much to scale the orbits
+    [AxisStateProperty]
+    public AxisState zAxis = new AxisState(0, 1, false, true, 50f, 0.1f, 0.1f, "Mouse ScrollWheel", false);
     // Declare variable for first person
     private bool firstPerson;
-    // Start is called before the first frame update
-    void Start()
+    // Declare variable for the mouse wheel input and zoom direction
+    private float mouseWheelInput;
+    private Vector3 zoomDirection;
+    void OnValidate()
+    {
+        minScale = Mathf.Max(0.01f, minScale);
+        maxScale = Mathf.Max(minScale, maxScale);
+    }
+    void Awake()
     {
         // Set firstPerson to true at start
         firstPerson = true;
+        if (thirdPersonCamera != null)
+        {
+            tpcOrbits = new CinemachineFreeLook.Orbit[thirdPersonCamera.m_Orbits.Length];
+            for (int i = 0; i < tpcOrbits.Length; i++)
+            {
+                tpcOrbits[i].m_Height = thirdPersonCamera.m_Orbits[i].m_Height;
+                tpcOrbits[i].m_Radius = thirdPersonCamera.m_Orbits[i].m_Radius;
+            }
+            SaveDuringPlay.SaveDuringPlay.OnHotSave -= RestoreTPCOrbits;
+            SaveDuringPlay.SaveDuringPlay.OnHotSave += RestoreTPCOrbits;
+        }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
     }
 
     // Update is called once per frame
@@ -22,11 +55,18 @@ public class CameraController : MonoBehaviour
     {
         // Call the SwitchCamera method
         SwitchCamera();
+        // Call the Zoom method
+        Zoom();
     }
     // Create a method to switch the active camera
     private void SwitchCamera()
     {
-        if(Input.GetKeyDown(KeyCode.F9))
+        // Set the mouseWheelInput
+        mouseWheelInput = Input.GetAxis("Mouse ScrollWheel");
+        if (firstPerson & mouseWheelInput < 0.0f)
+        {
+            firstPerson = !firstPerson;
+        } else if (!firstPerson & thirdPersonCamera.m_Orbits[1].m_Radius <= 5.0f)
         {
             firstPerson = !firstPerson;
         }
@@ -41,4 +81,50 @@ public class CameraController : MonoBehaviour
             thirdPersonCamera.Priority = 1;
         }
     }
+    // Create method to zoom the thirdPersonCamera in and out
+    private void Zoom()
+    {
+        if (tpcOrbits != null & !firstPerson)
+        {
+            zAxis.Update(Time.deltaTime);
+            float scale = Mathf.Lerp(minScale, maxScale, zAxis.Value);
+            for (int i = 0; i < tpcOrbits.Length; i++)
+            {
+                thirdPersonCamera.m_Orbits[i].m_Height = tpcOrbits[i].m_Height * scale;
+                thirdPersonCamera.m_Orbits[i].m_Radius = tpcOrbits[i].m_Radius * scale;
+            }
+        }
+    }
+    // Create method to return the tpcOrbits back to original position
+    private void RestoreTPCOrbits()
+    {
+        if (tpcOrbits != null)
+        {
+            for (int i = 0; i < tpcOrbits.Length; i++)
+            {
+                thirdPersonCamera.m_Orbits[i].m_Height = tpcOrbits[i].m_Height;
+                thirdPersonCamera.m_Orbits[i].m_Radius = tpcOrbits[i].m_Radius;
+            }
+        }
+    }
+    // Create method to restore the orginal orbits on scene destroy
+    private void OnDestroy()
+    {
+        SaveDuringPlay.SaveDuringPlay.OnHotSave -= RestoreTPCOrbits;
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
