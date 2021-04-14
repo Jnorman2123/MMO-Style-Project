dba3df275a1b340867e1a750691ed34c5ba39f8e
Jonathan Norman <Jnorman2123@gmail.com>
2021-04-14 14:26:51 -0400

    Player can now target objects in the scene with left mouse click
    Player can now target objects in the scene with left mouse click

---
+++ MMO-Style-Project/Assets/Scripts/PlayerTargeting.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerTargeting : MonoBehaviour
+{
+    // Declare variable for target
+    public GameObject target;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Call the MouseTarget method
+        MouseTarget();
+    }
+    // Create method that targets the object the player left clicks on
+    private void MouseTarget()
+    {
+        // When the mouse is clicked on an object target it and log its tag
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit))
+            {
+                target = (hit.transform.parent.gameObject);
+            } else
+            {
+                target = null;
+            }
+        }
+    }
+}
