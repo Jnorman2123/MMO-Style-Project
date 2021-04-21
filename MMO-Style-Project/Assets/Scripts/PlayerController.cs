ef06cf8687964238a5ff4d827a478a12791cf356
Jonathan Norman <Jnorman2123@gmail.com>
2021-04-21 12:03:40 -0400

    Created a player mana bar and updated player controller script
    Created a player mana bar and updated player controller script

--- MMO-Style-Project/Assets/Scripts/PlayerController.cs
+++ MMO-Style-Project/Assets/Scripts/PlayerController.cs
@@ -4,9 +4,11 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Declare variables for player current and max health
+    // Declare variables for player current and max health and mana
     public int currentHealth;
     public int maxHealth;
+    public int currentMana;
+    public int maxMana;
     // Declare variable for the player ui window 
     public GameObject playerUIWindow;
     // Start is called before the first frame update
@@ -15,8 +17,13 @@ public class PlayerController : MonoBehaviour
         // Set the max and current health
         maxHealth = 200;
         currentHealth = maxHealth;
+        // Set the max and current mana
+        maxMana = 100;
+        currentMana = maxMana;
         // Call the SetMaxHealthBarValue method
         playerUIWindow.GetComponent<PlayerWindowController>().SetMaxHealthBarValue();
+        // Call the SetMaxManaBarValue method
+        playerUIWindow.GetComponent<PlayerWindowController>().SetMaxManaBarValue();
     }
 
     // Update is called once per frame
@@ -27,10 +34,20 @@ public class PlayerController : MonoBehaviour
         {
             TakeDamage();
         }
+        // Call the UseMana method if the player presses the g key
+        if (Input.GetKeyDown(KeyCode.G))
+        {
+            UseMana();
+        }
     }
     // Create a method to damage the player to test the health bar
     private void TakeDamage()
     {
         currentHealth -= 50;
+    }
+    // Create method to simulate using mana to test mana bar
+    private void UseMana()
+    {
+        currentMana -= 10;
     }
 }
--- MMO-Style-Project/Assets/Scripts/PlayerWindowController.cs
+++ MMO-Style-Project/Assets/Scripts/PlayerWindowController.cs
@@ -12,11 +12,14 @@ public class PlayerWindowController : MonoBehaviour
     private string playerName = "Zidgog";
     // Declare variable for the player game object
     public GameObject playerObj;
-    // Declare variable for the player health bar slider
+    // Declare variables for the player health and mana bar sliders
     public Slider playerHealthBar;
-    // Declare variables for current and max health
+    public Slider playerManaBar;
+    // Declare variables for current and max health and mana
     private int maxHealth;
     private int currentHealth;
+    private int maxMana;
+    private int currentMana;
     // Start is called before the first frame update
     void Start()
     {
@@ -27,8 +30,10 @@ public class PlayerWindowController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        // Call the SetHealth method
+        // Call the SetHealthBarValue method
         SetHealthBarValue();
+        // Call the SetManaBarValue method
+        SetManaBarValue();
     }
     // Create a method to set the health bar health to the players current health
     private void SetHealthBarValue()
@@ -46,5 +51,22 @@ public class PlayerWindowController : MonoBehaviour
         // Set the playerHealthBar max and current values to maxHealth
         playerHealthBar.maxValue = maxHealth;
         playerHealthBar.value = maxHealth;
+    }
+    // Create a method to set the mana bar mana to the players current mana
+    private void SetManaBarValue()
+    {
+        // Set currentMana to the players current mana
+        currentMana = playerObj.GetComponent<PlayerController>().currentMana;
+        // Set the playerManaBar value to currentMana
+        playerManaBar.value = currentMana;
+    }
+    // Create a method to set the max value of the playerManaBar to the maxMana
+    public void SetMaxManaBarValue()
+    {
+        // Set the max and current mana of the player mana window
+        maxMana = playerObj.GetComponent<PlayerController>().maxMana;
+        // Set the playerManaBar max and current values to maxMana
+        playerManaBar.maxValue = maxMana;
+        playerManaBar.value = maxMana;
     }
 }
