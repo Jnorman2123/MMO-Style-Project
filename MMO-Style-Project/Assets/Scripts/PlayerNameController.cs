9638d4c333c00028e2cefdc42657eb8a0e7b982b
Jonathan Norman <Jnorman2123@gmail.com>
2021-04-22 20:39:48 -0400

    Added a canvas to display the player name over the player when targeted
    Added a canvas to display the player name over the player when targeted

---
+++ MMO-Style-Project/Assets/Scripts/PlayerNameController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using TMPro;
+using UnityEngine;
+
+public class PlayerNameController : MonoBehaviour
+{
+    // Declare variable for the player text mesh pro
+    public TextMeshProUGUI playerName;
+    // Declare variable for the player game object
+    public GameObject player;
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Call the ActivatePlayerName method
+        ActivatePlayerName();
+        
+    }
+    // Create a method to activate the player name when the player is targeted
+    private void ActivatePlayerName()
+    {
+        if (player.GetComponent<PlayerTargeting>().target == player)
+        {
+            playerName.text = player.name;
+        }
+        else
+        {
+            playerName.text = "";
+        }
+    }
+}
